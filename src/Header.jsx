59e91634ee45caa9e932f39422f6563fd78f3331
elizasj <eliza.struthersjobin@gmail.com>
2017-12-23 22:45:39 -0500

    update header

--- src/Header.jsx
+++ src/Header.jsx
@@ -12,7 +12,7 @@ const Header = props => {
         <AboutMe className='measure-wide'>
            <h1>{props.title}</h1>
            <p>I make things that live on the internet</p>
-           <p>I’m a creative who likes to experiment with sound visualisation and explore the many paradigms of perception by creating playful experiences in the browser. Originally from Montreal, Canada, I currently live in Paris, France and work as a Front-End Developer at <a className="b-highlight" href="https://www.scaleway.com/">Scaleway </a>. I’m also part of the new media art collective <a className="b-highlight" href="http://graffitiresearchlab.fr/">GRLFR</a>, and I write and run the creative coding blog <a className="b-highlight" href="http://www.unicornsfartpixels.com/">unicornsfartpixels.com</a>.</p>
+           <p>I’m a creative who likes to experiment with sound visualisation and explore the many paradigms of perception by creating playful experiences in the browser. Originally from Montreal, Canada, I currently live in Paris, France and work as a Front-End Developer at <a className="b-highlight" href="https://www.scaleway.com/">Scaleway </a>. I’m also part of the new media art collective <a className="b-highlight" href="http://graffitiresearchlab.fr/">GRLFR</a>, and I write and run the creative coding blog <a className="b-highlight" href="http://www.unicornsfartpixels.com/">unicornsfartpixels</a>.</p>
         </AboutMe>
     )
 }
